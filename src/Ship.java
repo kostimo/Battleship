7f5b18a9183965c8d51e0e4e56ab274d15a283fc
Kostiantyn Moroz <kostyamoroz50@gmail.com>
2024-11-25 23:01:14 +0100

    extenden attributes and added getters for them

--- src/Ship.java
+++ src/Ship.java
@@ -1,8 +1,17 @@
-//import java.util.ArrayList;
-import java.util.List;
 
 public class Ship {
     private int size;
-    private List<int> position;
+    private int direction;              // 0 for horizontal, 1 for vertical
+    private int[] position =new int[2]; //array with length 2 for ships (x, y) coordinates
     private boolean isSunk;
+
+    public int getSize() {
+        return size;
+    }
+    public int getDirection() {
+        return direction;
+    }
+    public int[] getPosition() {
+        return position;
+    }
 }
