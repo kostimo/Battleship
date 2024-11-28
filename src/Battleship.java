77de16b8d62309a7e69d0f979ff6f459e1e36595
Kostiantyn Moroz <kostyamoroz50@gmail.com>
2024-11-29 00:32:10 +0100

    modified constructor

--- src/Battleship.java
+++ src/Battleship.java
@@ -1,7 +1,5 @@
 public class Battleship extends Ship{
-    private final int size = 4;
-
     public Battleship(int x, int y, int direction) {
-        super(x, y, direction);
+        super(4, x, y, direction);
     }
 }
--- src/Destroyer.java
+++ src/Destroyer.java
@@ -1,7 +1,5 @@
 public class Destroyer extends Ship {
-    private final int size = 2;
-
     public Destroyer(int x, int y, int direction) {
-        super(x, y, direction);
+        super(2, x, y, direction);
     }
 }
