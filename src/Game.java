33a42033288a7bf1627b79fee95eeed15670158b
Kostiantyn Moroz <kostyamoroz50@gmail.com>
2024-11-28 15:57:18 +0100

    added test greeting method

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,7 @@
 public class Game {
+    public static void greeting()
+    {
+        System.out.println("Welcome to the Battlship game!\nThis is a game between 2 players.\n" +
+                            "Firstly, each player need to locate their 5 ships.\n");
+    }
 }
