66def016872e4c3f81f3c799ec66b99be90b4ec5
Kostiantyn Moroz <kostyamoroz50@gmail.com>
2024-12-06 09:57:40 +0100

    new method signatures

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,13 @@
 public class Game {
-    public static void greeting()
+    public static void welcome()
     {
         System.out.println("Welcome to the Battlship game!\nThis is a game between 2 players.\n" +
                             "Firstly, each player need to locate their 5 ships.\n");
+    }
+    public static void greetWinner() {}
+    public static boolean checkWinner() {return false;}
+    public static void placeShip(Player player)
+    {
+
     }
 }
