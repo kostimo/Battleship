b5e3fa4e2bacb1f841484c0074a988cd3e556194
Kostiantyn Moroz <kostyamoroz50@gmail.com>
2024-12-06 10:00:23 +0100

    some new lines to test Player.placeShip()

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,54 @@
 public class Main {
     public static void main(String[] args) {
-        Player player1 = new Player();
-        GameBoard board1 = new GameBoard();
-        Game.greeting();
-        board1.printBoard();
+        Player player1 = new Player("Hans");
+        Game.welcome();
+        player1.getBoard().printBoard();
         
         Battleship bs1 = new Battleship(0, 0, 0);
 //        bs1.fillShipCellArray();
-        bs1.printShipCells();
+//        bs1.printShipCells();
 
-        System.out.println();
+//        System.out.println();
 
         Battleship bs2 = new Battleship(9, 0, 1);
 //        bs2.fillShipCellArray();
-        bs2.printShipCells();
+//        bs2.printShipCells();
 
-        System.out.println();
+//        System.out.println();
 
         Battleship bs3 = new Battleship(4, 4, 0);
 //        bs3.fillShipCellArray();
-        bs3.printShipCells();
+//        bs3.printShipCells();
 
-        System.out.println();
+//        System.out.println();
 
         Destroyer d1   = new Destroyer(0, 2, 1);
 //        d1.fillShipCellArray();
-        d1.printShipCells();
+//        d1.printShipCells();
 
-        System.out.println();
+//        System.out.println();
 
         Destroyer d2   = new Destroyer(8, 9, 0);
 //        d2.fillShipCellArray();
-        d2.printShipCells();
+//        d2.printShipCells();
 
-        System.out.println(board1.placeShip(bs1));
-        System.out.println(board1.placeShip(bs2));
-        System.out.println(board1.placeShip(bs3));
-        System.out.println(board1.placeShip(d1));
-        System.out.println(board1.placeShip(d2));
+//        System.out.println(player1.getBoard().placeShip(bs1));
+//        System.out.println(player1.getBoard().placeShip(bs2));
+//        System.out.println(player1.getBoard().placeShip(bs3));
+//        System.out.println(player1.getBoard().placeShip(d1));
+//        System.out.println(player1.getBoard().placeShip(d2));
+//
+//        player1.getBoard().updateShipsOnBoard();
+//        player1.getBoard().printBoard();
 
-        board1.updateShipsOnBoard();
-        board1.printBoard();
+        for (int i=0; i<5; i++)
+        {
+            Ship ship = player1.askForShip();
+            ship.printShipCells();
+            player1.placeShip(ship);
+        }
+        player1.getBoard().updateShipsOnBoard();
+        player1.getBoard().printBoard();
 
         /*
         * Welcome to the Battlship game! This is a game between 2 players.
