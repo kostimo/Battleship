5a41c9d454cd724f7b2850b250975e79ca16812b
Kostiantyn Moroz <kostyamoroz50@gmail.com>
2024-12-10 22:54:29 +0100

    shipsOnBoard is now ArrayList. New symbol '*'

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,9 +1,9 @@
+import java.util.ArrayList;
+
 public class GameBoard {
     private final int ROWS =10, COLS =10;
-    private String[][] grid =new String[ROWS][COLS];
-
-    private int counter =0;                   // for tracking a position in a ships array
-    private Ship[] shipsOnBoard =new Ship[5]; //board has 5 ships on it
+    private final String[][] grid =new String[ROWS][COLS];
+    private final ArrayList<Ship> shipsOnBoard =new ArrayList<>(5); //board has 5 ships on it
 
     public GameBoard()
     {
@@ -14,57 +14,30 @@ public class GameBoard {
     {
         for (int i=0; i<ROWS; i++)
         {
-            for (int j=0; j<COLS; j++)
-            {
-                    grid[i][j] = "~";
-            }
+            for (int j=0; j<COLS; j++) { grid[i][j] ="~"; }
         }
     }
-
-//    public void updateBoard()
-//    {
-//        updateShipsOnBoard();
-//        fillBoardWithWater();
-//    }
-
-    //fills grid with water wherever the cells are not occupied with ships
-//    private void fillBoardWithWater()
-//    {
-//        for (int i=0; i<ROWS; i++)
-//        {
-//            for (int j=0; j<COLS; j++)
-//            {
-//                if (grid[i][j].isEmpty())
-//                {
-//                    grid[i][j] = "~";
-//                }
-//            }
-//        }
-//    }
-
     public void updateShipsOnBoard()
     {
         for (Ship ship: shipsOnBoard)     // traverses all ships on the board
         {
-            shipCell[] cells = ship.getShipCells();
-//            System.out.println(cells[0].getX() + " " + cells[0].getY());
-            for (shipCell cell: cells)           // traverses shipCells within each ship
+            for (shipCell cell: ship.getShipCells())    // traverses shipCell's within each ship
             {
                 int x =cell.getX();
                 int y =cell.getY();
-                if (cell.getIsDamaged())
-                {
-                    this.grid[y][x] ="X";
-                } else {
-                    this.grid[y][x] ="■";
+                if (ship.isSunk()) this.grid[y][x] ="X";
+                else {
+                    if (cell.getIsDamaged()) this.grid[y][x] = "*";
+                    else this.grid[y][x] = "■";
                 }
             }
         }
     }
-    public void printBoard()
+    public void printBoard(Player player)
     {
+        System.out.printf("\n%s's current board:\n", player.getName());
         System.out.print("  ");
-        for (int i = 1; i <= ROWS; i++) {
+        for (int i = 1; i <= COLS; i++) {
             System.out.print(" " + i + " ");
         }
         System.out.println();
@@ -72,38 +45,17 @@ public class GameBoard {
         for (int i=0; i<ROWS; i++)
         {
             switch (i) {
-                case 0:
-                    System.out.print("A  ");
-                    break;
-                case 1:
-                    System.out.print("B  ");
-                    break;
-                case 2:
-                    System.out.print("C  ");
-                    break;
-                case 3:
-                    System.out.print("D  ");
-                    break;
-                case 4:
-                    System.out.print("E  ");
-                    break;
-                case 5:
-                    System.out.print("F  ");
-                    break;
-                case 6:
-                    System.out.print("G  ");
-                    break;
-                case 7:
-                    System.out.print("H  ");
-                    break;
-                case 8:
-                    System.out.print("I  ");
-                    break;
-                case 9:
-                    System.out.print("J  ");
-                    break;
-                default:
-                    System.out.print("   ");
+                case 0 -> System.out.print("A  ");
+                case 1 -> System.out.print("B  ");
+                case 2 -> System.out.print("C  ");
+                case 3 -> System.out.print("D  ");
+                case 4 -> System.out.print("E  ");
+                case 5 -> System.out.print("F  ");
+                case 6 -> System.out.print("G  ");
+                case 7 -> System.out.print("H  ");
+                case 8 -> System.out.print("I  ");
+                case 9 -> System.out.print("J  ");
+                default -> System.out.print("   ");
             }
             for (int j=0; j<COLS; j++)
             {
@@ -116,56 +68,47 @@ public class GameBoard {
     public boolean shootShip(Player player, int x, int y)
     {
         switch (grid[y][x]) {
-            case "~":
-                return false;
-            case "S":
-            {
-                // searching for the shipCell
+            case "~":  return false;
+            case "■":
+                // traversing shipCells
                 for (Ship ship : this.shipsOnBoard) {
-                    shipCell[] cells = ship.getShipCells();
-                    for (shipCell cell : cells) {
+                    for (shipCell cell : ship.getShipCells()) {
+                        // there is a shipCell whose coordinates match up with the grid cell that's being shot
                         if (cell.getX() == x && cell.getY() == y) {
                             cell.setIsDamaged();
                             return true;
                         }
                     }
                 }
-            }
-            default:
+            case "*":
                 player.setSecondTry(true);
                 return false;
         }
+        // normally it shouldn't be executed;
+        System.out.println("GameBoard.shootShip() error");
+        return false;
     }
 
 
     // FUNCTION DOESNT CHECK SHIP OVERLAPPING YET
     public boolean placeShip(Ship ship) {
-        switch (ship.getDirection()) {
-            case 0: // if ship is horizontally
-                if (ship.getPosition()[0] + ship.getSize() - 1 >= COLS || ship.getPosition()[1] >= ROWS) {
-                    return false;
-                } else {
-                    this.addShip(ship);
-                    return true;
-                }
-            case 1: // if ship is vertically
-                if (ship.getPosition()[1] + ship.getSize() - 1 >= ROWS || ship.getPosition()[0] >= COLS) {
-                    return false;
-                } else {
-                    this.addShip(ship);
-                    return true;
-                }
-            default:
+        // if ship is vertical
+        if (ship.getVertical()) {
+            if (ship.getPosition()[1] + ship.getSize() - 1 >= ROWS || ship.getPosition()[0] >= COLS) {
                 return false;
+            } else {
+                shipsOnBoard.add(ship);
+                return true;
+            }
         }
-    }
-
-    private void addShip(Ship ship)
-    {
-        shipsOnBoard[counter++] =ship;
-    }
-
-    public boolean isHit() {
-        return false;
+        // if ship is horizontal
+        else {
+            if (ship.getPosition()[0] + ship.getSize() - 1 >= COLS || ship.getPosition()[1] >= ROWS) {
+                return false;
+            } else {
+                shipsOnBoard.add(ship);
+                return true;
+            }
+        }
     }
 }
