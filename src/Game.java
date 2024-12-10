6f253683df3cbc918871d9a5e8f228cfdc46d0a7
Kostiantyn Moroz <kostyamoroz50@gmail.com>
2024-12-10 23:01:43 +0100

    updated

--- src/Game.java
+++ src/Game.java
@@ -1,9 +1,13 @@
 public class Game {
     public static void welcome()
     {
-        System.out.println("Welcome to the Battlship game!\nThis is a game between 2 players.\n" +
-                            "Firstly, each player need to locate their 5 ships.\n");
+        System.out.println("""
+                Welcome to the Battleship game!
+                This is a game between 2 players.
+                Firstly, each player need to enter their names and then locate 5 ships.
+                """);
     }
+    public static void askForName() {}
     public static void greetWinner() {}
     public static boolean checkWinner() {return false;}
     public static void placeShip(Player player)
