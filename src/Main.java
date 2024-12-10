fcceef6946741b36c1839418aef29eed196ce04d
Kostiantyn Moroz <kostyamoroz50@gmail.com>
2024-12-10 22:55:07 +0100

    testing code

--- src/Main.java
+++ src/Main.java
@@ -2,72 +2,56 @@ public class Main {
     public static void main(String[] args) {
         Player player1 = new Player("Hans");
         Game.welcome();
-        player1.getBoard().printBoard();
-        
-        Battleship bs1 = new Battleship(0, 0, 0);
-//        bs1.fillShipCellArray();
-//        bs1.printShipCells();
+        player1.getBoard().printBoard(player1);
 
-//        System.out.println();
+        // CREATING SHIPS
+        Battleship bs1 = new Battleship(0, 0, false);
+        Battleship bs2 = new Battleship(9, 0, true);
+        Battleship bs3 = new Battleship(4, 4, false);
+        Destroyer d1   = new Destroyer(0, 2, true);
+        Destroyer d2   = new Destroyer(8, 9, false);
 
-        Battleship bs2 = new Battleship(9, 0, 1);
-//        bs2.fillShipCellArray();
-//        bs2.printShipCells();
+        // PLACING SHIPS
+        player1.placeShip(bs1);
+        player1.placeShip(bs2);
+        player1.placeShip(bs3);
+        player1.placeShip(d1);
+        player1.placeShip(d2);
 
-//        System.out.println();
+        // UPDATING & PRINTING PLAYER'S BOARD
+        player1.getBoard().updateShipsOnBoard();
+        player1.getBoard().printBoard(player1);
 
-        Battleship bs3 = new Battleship(4, 4, 0);
-//        bs3.fillShipCellArray();
-//        bs3.printShipCells();
-
-//        System.out.println();
-
-        Destroyer d1   = new Destroyer(0, 2, 1);
-//        d1.fillShipCellArray();
-//        d1.printShipCells();
-
-//        System.out.println();
-
-        Destroyer d2   = new Destroyer(8, 9, 0);
-//        d2.fillShipCellArray();
-//        d2.printShipCells();
-
-//        System.out.println(player1.getBoard().placeShip(bs1));
-//        System.out.println(player1.getBoard().placeShip(bs2));
-//        System.out.println(player1.getBoard().placeShip(bs3));
-//        System.out.println(player1.getBoard().placeShip(d1));
-//        System.out.println(player1.getBoard().placeShip(d2));
-//
+        // PLAYER'S INPUT TO PLACE SHIPS
+//        for (int i=0; i<5; i++)
+//        {
+//            Ship ship = player1.askForShip();
+//            ship.printShipCells();
+//            player1.placeShip(ship);
+//        }
 //        player1.getBoard().updateShipsOnBoard();
 //        player1.getBoard().printBoard();
 
-        for (int i=0; i<5; i++)
-        {
-            Ship ship = player1.askForShip();
-            ship.printShipCells();
-            player1.placeShip(ship);
-        }
+        // PLAYER'S SHOOTING
+        player1.shoot(0, 0);
         player1.getBoard().updateShipsOnBoard();
-        player1.getBoard().printBoard();
+        player1.getBoard().printBoard(player1);
 
-        /*
-        * Welcome to the Battlship game! This is a game between 2 players.
-        * Firstly, each player need to locate their 5 ships.
-        *
-        * Player 1. Please give me a type of the ship and its coordinates in form "d 3 4 0" where d - ship type, 3 and 4 - x and y coordinates, 0 - horizontal direction (1 - vertical)
-                    * if location ok -> "Succeded! n ships more"
-                    * otherwise      -> "Please, try again"
-        *
-        * player1.placeShip("d", 3, 4, 0)
-        * player1.placeShip("d", 0, 9, 1)
-        * player1.placeShip("b", 7, 6, 1)
-        * player1.placeShip("b", 8, 4, 0)
-        * player1.placeShip("d", 9, 0, 0)
-        *
-        * Generally we need to placeShip (to add it to the boards list of ships)
-        *
-        * now its players 2 turn
-        *
-        * */
+        player1.shoot(0, 0);
+        player1.getBoard().updateShipsOnBoard();
+        player1.getBoard().printBoard(player1);
+
+        player1.shoot(1, 0);
+        player1.getBoard().updateShipsOnBoard();
+        player1.getBoard().printBoard(player1);
+
+        player1.shoot(2, 0);
+        player1.getBoard().updateShipsOnBoard();
+        player1.getBoard().printBoard(player1);
+
+        player1.shoot(3, 0);
+        player1.getBoard().updateShipsOnBoard();
+        player1.getBoard().printBoard(player1);
+
     }
 }
