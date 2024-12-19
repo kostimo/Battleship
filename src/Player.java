d1152ab902c36ddd7cf9238fe6ffc990efa7c175
selina <diamondsese@raumdock.de>
2024-12-19 22:58:33 +0100

    added Java-Doc

--- src/Player.java
+++ src/Player.java
@@ -1,16 +1,34 @@
 import java.io.InputStream;
 import java.util.Scanner;
 
+/**
+ * Represents a player in the Battleship game.
+ * The player can place ships and shoot at ships on the game board.
+ */
 public class Player implements PlayerAction {
     private String name;
     private boolean secondTry;   // in case player shoots a cell that has already been damaged
     private final GameBoard board =new GameBoard();
+
+    /**
+     * Places a ship on the player's game board.
+     *
+     * @param ship {@link Ship}-object
+     */
     @Override
     public void placeShip(Ship ship) {
         boolean success = this.board.placeShip(ship);
         if (success) System.out.print("Success! The ship is placed.");
         else System.out.print("Error! The ship couldn't be placed.");
     }
+
+    /**
+     * Shoots at a specific position on the game board.
+     * It also checks if the shot was successful, if the player has already shot there, or if it missed.
+     *
+     * @param x x-coordinate of the shot
+     * @param y y-coordinate of the shot
+     */
     @Override
     public void shoot(int x, int y) {
         this.secondTry =false;
@@ -22,18 +40,47 @@ public class Player implements PlayerAction {
         else                     System.out.println("Oh no, you have missed!\n\n");
     }
 
+    /**
+     * Gets the game board
+     *
+     * @return current board as a {@link GameBoard}-object
+     */
     public GameBoard getBoard() {
         return board;
     }
+
+    /**
+     * Sets the value of {@code secondTry} to indicate if the player is retrying a shot.
+     *
+     * @param secondTry true if the player gets a second shot, otherwise false
+     */
     public void setSecondTry(boolean secondTry) {
         this.secondTry = secondTry;
     }
+
+    /**
+     * Checks if the player is on his second try.
+     *
+     * @return true if it is indeed the second try, false otherwise
+     */
     public boolean isSecondTry() {
         return secondTry;
     }
+
+    /**
+     * Gets the player's name
+     *
+     * @return name of the player as a {@code String}
+     */
     public String getName() {
         return name;
     }
+
+    /**
+     * Sets the name of the player as a {@code String}
+     *
+     * @param name the player's name
+     */
     public void setName(String name) {
         this.name = name;
     }
--- src/shipCell.java
+++ src/shipCell.java
@@ -1,35 +1,66 @@
-// Because a Ship has size and takes several grid cells, we could need such class as shipCell
-// 1. Ship will consist of some shipCells which can have different state (e.g. damaged or not damaged)
-// 2. Also, it can be helpful because now we can check for example if all shipCells are damaged, in this case
-//    we can mark the Ship as sunk.
-
+/**
+ * Represents a single part of a ship.
+ * Each cell has specific coordinates and a status which tells us whether the ship cell is damaged or not.
+ * This allows us to check if the ship has sunk if all of its cells are damaged.
+ */
 public class shipCell {
     private final int x;
     private final int y;
     private boolean isDamaged =false;
 
+    /**
+     * Constructs a ship cell at the stated coordinates.
+     * Initially, the cell is not damaged.
+     *
+     * @param x x-coordinate
+     * @param y y-coordinate
+     */
     public shipCell(int x, int y)
     {
         this.x =x;
         this.y =y;
     }
+
+    /**
+     * Prints out all the values of a ship cell.
+     */
     public void printCell()
     {
         System.out.println("shipCell: " + x + " " + y + " " + isDamaged);
     }
 
+    /**
+     * Gets the status of a ship cell.
+     *
+     * @return true when damaged, false when not
+     */
     public boolean getIsDamaged()
     {
         return isDamaged;
     }
+
+    /**
+     * Sets the ship cell state to 'damaged'.
+     */
     public void setIsDamaged()
     {
         this.isDamaged =true;
     }
+
+    /**
+     * Gets the x-coordinate of the ship cell
+     * @return x-coordinate
+     */
     public int getX()
     {
         return this.x;
     }
+
+    /**
+     *Gets the y-coordinate of the ship cell
+     *
+     * @return y-coordinate
+     */
     public int getY() {
         return this.y;
     }
