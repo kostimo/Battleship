a49e3ccd46aa06f46a98893b66dbec8b6462cc9b
selina <diamondsese@raumdock.de>
2024-12-19 17:45:51 +0100

    added Java-Doc

--- src/Ship.java
+++ src/Ship.java
@@ -1,11 +1,23 @@
 import java.util.ArrayList;
 
+/**
+ * This abstract class represents a ship.
+ * A ship is made of several cells and can be placed horizontally or vertically by the players.
+ */
 public abstract class Ship {
     private final int size;
     private final boolean isVertical;
     private final int[] position =new int[2]; //array with length 2 for ships (x, y) coordinates
     private final ArrayList<shipCell> shipCells;
 
+    /**
+     * This is the constructor for a ship.
+     *
+     * @param size: size of the ship (number of cells)
+     * @param x: x-coordinate (0 - 9)
+     * @param y: y-coordinate (0 - 9)
+     * @param isVertical: direction the ship is facing; if the ship is vertical, the value is true, otherwise false
+     */
     public Ship(int size, int x, int y, boolean isVertical)
     {
         this.size        =size;
@@ -15,6 +27,11 @@ public abstract class Ship {
         this.isVertical  =isVertical;
         fillShipCellArray();
     }
+
+    /**
+     * This method creates {@link shipCell}-objects, where a ship is placed.
+     * The ship cells are placed horizontally or vertically on the grid depending on the direction of the ship.
+     */
     private void fillShipCellArray()
     {
         if (this.isVertical)     // vertical direction
@@ -31,6 +48,13 @@ public abstract class Ship {
             }
         }
     }
+
+    /**
+     * This method checks, if the ship is sunk.
+     * A ship is sunk when every ship cell is damaged.
+     *
+     * @return true, if the ship is sunk, otherwise false
+     */
     public boolean isSunk()
     {
         boolean isSunk = true;
@@ -44,14 +68,41 @@ public abstract class Ship {
         }
         return isSunk;
     }
+
+    /**
+     * prints ship cells
+     */
     public void printShipCells()
     {
         System.out.println("shipCells:");
         for (shipCell cell: shipCells) { cell.printCell(); }
     }
 
+    /**
+     * getter for the ship size
+     *
+     * @return size of the ship (number of cells e.g. 2)
+     */
     public int getSize() { return size; }
+
+    /**
+     * getter for the direction
+     *
+     * @return true for vertical or false for horizontal
+     */
     public boolean getVertical() { return isVertical; }
+
+    /**
+     * getter for the position
+     *
+     * @return x and y coordinates
+     */
     public int[] getPosition() { return position; }
+
+    /**
+     * getter for a list of the ship cells
+     *
+     * @return a list containing {@link shipCell}-objects
+     */
     public ArrayList<shipCell> getShipCells() { return shipCells; }
 }
