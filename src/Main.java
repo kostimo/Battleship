5aa2d15b71e7d645a1967ffd433ea945b59b32ac
Kostiantyn Moroz <kostyamoroz50@gmail.com>
2024-12-19 15:14:02 +0100

    added game loop

--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,48 @@
 public class Main {
     public static void main(String[] args) {
-        Player player1 = new Player("Hans");
+        Player[] players = new Player[2];
+        players[0] = new Player();
+        players[1] = new Player();
+
         Game.welcome();
-        player1.getBoard().printBoard(player1);
+        while (true) {
 
-        // CREATING SHIPS
-        Battleship bs1 = new Battleship(0, 0, false);
-        Battleship bs2 = new Battleship(9, 0, true);
-        Battleship bs3 = new Battleship(4, 4, false);
-        Destroyer d1   = new Destroyer(0, 2, true);
-        Destroyer d2   = new Destroyer(8, 9, false);
+            int iteration = Game.getIteration();
+            Player currentPlayer = (iteration % 2 == 0) ? players[0] : players[1];
+            Player nextPlayer =((iteration+1) % 2 == 0) ? players[0] : players[1];
 
-        // PLACING SHIPS
-        player1.placeShip(bs1);
-        player1.placeShip(bs2);
-        player1.placeShip(bs3);
-        player1.placeShip(d1);
-        player1.placeShip(d2);
+            // player registration, board setup
+            if (iteration < 2) {
+                Game.askForName(currentPlayer);
+                for (int i = 0; i < 5; i++) {
+                    Game.showBoard(currentPlayer);
+                    Game.askForShip(currentPlayer);
+                }
+                Game.showBoard(currentPlayer);
+                System.out.print("\nGreat, my capitan! All ships are successfully placed!\n\n");
 
-        // UPDATING & PRINTING PLAYER'S BOARD
-        player1.getBoard().updateShipsOnBoard();
-        player1.getBoard().printBoard(player1);
+            // battleship begins
+            } else
+            {
+                // one-time message in the beginning
+                if (iteration == 2) System.out.print("\nMay BATTLESHIP begin!\n");
+                Game.printPlayerHeader(currentPlayer);
+                System.out.print("\nEnemy board\n");
+                Game.showBoard(nextPlayer);
+                Game.askToShoot(currentPlayer);
 
-        // PLAYER'S INPUT TO PLACE SHIPS
-//        for (int i=0; i<5; i++)
-//        {
-//            Ship ship = player1.askForShip();
-//            ship.printShipCells();
-//            player1.placeShip(ship);
-//        }
-//        player1.getBoard().updateShipsOnBoard();
-//        player1.getBoard().printBoard();
+                // winner checking after the turn
+                int winner =Game.checkWinner(players);
+                if (winner != -1)
+                {
+                    Game.greetWinner(players[winner]);
+                    break;
+                }
+            }
 
-        // PLAYER'S SHOOTING
-        player1.shoot(0, 0);
-        player1.getBoard().updateShipsOnBoard();
-        player1.getBoard().printBoard(player1);
+            Game.increment();
+        }
 
-        player1.shoot(0, 0);
-        player1.getBoard().updateShipsOnBoard();
-        player1.getBoard().printBoard(player1);
-
-        player1.shoot(1, 0);
-        player1.getBoard().updateShipsOnBoard();
-        player1.getBoard().printBoard(player1);
-
-        player1.shoot(2, 0);
-        player1.getBoard().updateShipsOnBoard();
-        player1.getBoard().printBoard(player1);
-
-        player1.shoot(3, 0);
-        player1.getBoard().updateShipsOnBoard();
-        player1.getBoard().printBoard(player1);
-
+        System.out.print("\n---------------GAME OVER---------------\n");
     }
 }
