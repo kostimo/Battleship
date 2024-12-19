ca8d601a20d2ce4a23cc029eb7f7b4cbcb7bd47b
Kostiantyn Moroz <kostyamoroz50@gmail.com>
2024-12-19 15:13:29 +0100

    added game loop logic

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
+
 public class Game {
+    private static int iteration =0; //when iteration % 2 == 0, it is player's 1 turn. Otherwise, player's 2
+    public static void increment() {
+        iteration++;
+    }
+    public static int getIteration()
+    {
+        return iteration;
+    }
+
     public static void welcome()
     {
         System.out.println("""
@@ -7,9 +18,74 @@ public class Game {
                 Firstly, each player need to enter their names and then locate 5 ships.
                 """);
     }
-    public static void askForName() {}
-    public static void greetWinner() {}
-    public static boolean checkWinner() {return false;}
+    public static void printPlayerHeader(Player player)
+    {
+        if (player == null) System.out.printf("------- PLAYER %d -------\n", iteration%2 == 0 ? 1 : 2);
+        else System.out.printf("------- PLAYER %d: %s -------\n", iteration%2 == 0 ? 1 : 2, player.getName());
+    }
+    public static void askForName(Player player) {
+        printPlayerHeader(null);
+        System.out.print("Enter your name, capitan!\n" +
+                            "-> ");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        player.setName(name);
+        System.out.printf("Hello, %s!", player.getName());
+    }
+    public static void showBoard(Player player)
+    {
+        System.out.printf("\n\n%s's current board:\n", player.getName());
+        player.getBoard().updateShipsOnBoard();
+        player.getBoard().printBoard();
+    }
+    public static void askForShip(Player player)
+    {
+        Scanner sc = new Scanner(System.in);
+        if (player.getBoard().getShipsNum() == 0) System.out.print("\nTime for you to create ships!");
+        System.out.printf("\n%d ships left!", 5-player.getBoard().getShipsNum());
+        System.out.print("""
+                \nTo create a ship enter a ship type, destroyer (d) or battleship (b),
+                ship orientation, horizontal (h) or vertical (v), row (A-J) and column (0-9)\n
+                """);
+
+        System.out.print("Ship type: ");
+        char type = sc.next().charAt(0);
+        System.out.print("Ship orientation: ");
+        char orientation = sc.next().charAt(0);
+        System.out.print("Row: ");
+        char row = sc.next().charAt(0);
+        System.out.print("Column: ");
+        int column = sc.nextInt();
+
+        Ship ship = (type == 'd')
+                ? new Destroyer(column, (int)row-65, orientation == 'v')
+                : new Battleship(column, (int)row-65, orientation == 'v');
+        player.placeShip(ship);
+    }
+    public static void askToShoot(Player player)
+    {
+        System.out.print("\nMy capitan, send this bastard to the bottom!");
+        System.out.print("\n(first character is the row, second - the column, without space)");
+        System.out.print("\nYour coordinates: ");
+        Scanner sc = new Scanner(System.in);
+        char row = sc.next().charAt(0);
+        int column = sc.nextInt();
+        player.shoot(column, row-65);
+    }
+    public static void greetWinner(Player winner)
+    {
+        System.out.print("\n@@@@          @@@@@@@@@@@@@@@@         @@@@");
+        System.out.printf("\nCongrats, %s! It was a brilliant battle.", winner.getName());
+        System.out.print("\n@@@@          @@@@@@@@@@@@@@@@         @@@@");
+    }
+    public static int checkWinner(Player[] players)
+    {
+        for (int i=0; i< players.length; i++)
+        {
+            if (players[i].getBoard().areAllShipsSunk()) return (i+1)%2; // returns index of winner
+        }
+        return -1; // when there is no winner yet
+    }
     public static void placeShip(Player player)
     {
 
