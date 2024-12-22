0d2d0810b322f2132afc71c61d193e9c736ad131
selina <diamondsese@raumdock.de>
2024-12-22 21:23:05 +0100

    re-added JavaDoc

--- src/Validation.java
+++ src/Validation.java
@@ -1,12 +1,32 @@
 import java.util.ArrayList;
 
+/**
+ * Contains methods that check if the entered coordinates for ship placement and shooting are within range and if the
+ * placed ships are overlapping.
+ * <p>Since all the methods are static, they can be used without creating an object of this class.</p>
+ */
 public class Validation {
+
+    /**
+     * Checks if the shooting coordinates are within range.
+     * @param x x-coordinate
+     * @param y y-coordinate
+     * @return true if {@code x} is between 0 and 9 and {@code y} is between 'A' and 'J', false otherwise
+     */
     public static boolean checkShootCoordinates(int x, int y)
     {
         if (x >= 0 && x <= 9) return (y >= 'A'-65 && y <= 'J'-65);
         return false;
     }
-    //returns true if coordinates valid
+
+    /**
+     * Checks the validity of the ship placement.
+     *
+     * @param ship The ship that gets checked
+     * @param rows number of rows on the board
+     * @param cols number of columns on the board
+     * @return true if the coordinates are valid, false otherwise
+     */
     public static boolean checkShipCoordinates(Ship ship, int rows, int cols)
     {
         // if ship is vertical
@@ -18,6 +38,14 @@ public class Validation {
         else return (ship.getPosition()[0] + ship.getSize() - 1 < cols && ship.getPosition()[1] < rows &&
                      ship.getPosition()[0] >=0                         && ship.getPosition()[1] >= 0);
     }
+
+    /**
+     * Checks if the new ship is overlapping with any ship on the board.
+     *
+     * @param shipFromInput the ship to check
+     * @param boardShips list of ships already placed on the board
+     * @return true if the entered ship is overlapping with another ship, otherwise false
+     */
     public static boolean shipsOverlapping(Ship shipFromInput, ArrayList<Ship> boardShips)
     {
 //      shipOverlapping =false;
@@ -27,6 +55,14 @@ public class Validation {
         }
         return false;
     }
+
+    /**
+     * Checks if two ships are overlapping.
+     *
+     * @param shipFromInput entered ship to check
+     * @param shipFromBoard already placed ship
+     * @return true if the ships overlap, false otherwise
+     */
     private static boolean shipAndShipOverlapping(Ship shipFromInput, Ship shipFromBoard)
     {
         for (shipCell cellInputShip: shipFromInput.getShipCells())
