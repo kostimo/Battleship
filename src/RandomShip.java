eb6a04498057e8c765a2c5b2978ba6e455396996
selina <diamondsese@raumdock.de>
2024-12-22 21:31:14 +0100

    re-added JavaDoc

--- src/RandomShip.java
+++ src/RandomShip.java
@@ -1,23 +1,52 @@
 import java.util.Random;
 
+/**
+ * Represents a computer generated ship.<br>
+ * It extends the {@link Ship} class and generates a ship with a random size, position, and orientation.
+ */
 public class RandomShip extends Ship
 {
     private static final Random random = new Random();
+
+    /**
+     * Constructs a random ship with random size, coordinates, and orientation.
+     */
     public RandomShip() {
         super(generateRandomSize(), generateRandomX(), generateRandomY(), generateRandomOrientation());
     }
+
+    /**
+     * Generates a random size.
+     *
+     * @return 2 or 4
+     */
     private static int generateRandomSize() {
         return random.nextBoolean() ? 2 : 4;
     }
 
+    /**
+     * Generates a random x-coordinate.
+     *
+     * @return random number between 0 and 9
+     */
     private static int generateRandomX() {
         return random.nextInt(10);
     }
 
+    /**
+     * Generates a random y-coordinate.
+     *
+     * @return random number between 0 and 9
+     */
     private static int generateRandomY() {
         return random.nextInt(10);
     }
 
+    /**
+     * Generates a random orientation.
+     *
+     * @return true or false
+     */
     private static boolean generateRandomOrientation() {
         return random.nextBoolean();
     }
